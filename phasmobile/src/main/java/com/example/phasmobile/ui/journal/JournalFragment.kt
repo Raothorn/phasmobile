6fa681bc39f6f1ec6e2726e5dd3fcfabe652b241
Gaston Gilbert <gastongator@gmail.com>
2023-10-20 19:37:15 -0500

    journal ui update

--- phasmobile/src/main/java/com/example/phasmobile/ui/journal/JournalFragment.kt
+++ phasmobile/src/main/java/com/example/phasmobile/ui/journal/JournalFragment.kt
@@ -1,18 +1,12 @@
 package com.example.phasmobile.ui.journal
 
-import androidx.lifecycle.ViewModelProvider
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
-import com.example.phasmobile.R
 import com.example.phasmobile.databinding.FragmentJournalBinding
 
 private const val TAG = "Interface"
@@ -20,74 +14,44 @@ class JournalFragment : Fragment() {
 
     private var _binding: FragmentJournalBinding? = null
     private val binding get() = _binding!!
-    companion object {
-        fun newInstance() = JournalFragment()
-    }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        val viewModel = JournalViewModel()
+        val viewModel = JournalViewModel.getInstance()
         _binding = FragmentJournalBinding.inflate(inflater, container, false)
         binding.viewModel = viewModel
         val root = binding.root
 
-        val customAdapter = CustomAdapter()
+        val tvs = hashMapOf(
+            "Spirit" to binding.tvSpirit,
+            "Poltergeist" to binding.tvPoltergeist,
+            "Banshee" to binding.tvBanshee,
+            "Jinn" to binding.tvJinn,
+            "Mare" to binding.tvMare,
+            "Revenant" to binding.tvRevenant,
+            "Shade" to binding.tvShade,
+            "Demon" to binding.tvDemon,
+            "Yurei" to binding.tvYurei,
+            "Oni" to binding.tvOni,
+            "Wraith" to binding.tvWraith,
+            "Phantom" to binding.tvPhantom
+        )
 
-        val recycleView: RecyclerView = binding.rvGhosts;
-        recycleView.layoutManager = LinearLayoutManager(context)
-        recycleView.adapter = customAdapter
-        Log.d(TAG, "Adapter attached")
-
-        viewModel.ghosts.observe(viewLifecycleOwner
-        ) { newGhosts ->
-            run {
-                customAdapter.submitList(newGhosts)
+        viewModel.ghosts.observe(viewLifecycleOwner) { newGhosts ->
+            Log.i(TAG, "Ghosts changed: $newGhosts")
+            for ((name, tvId) in tvs) {
+                val alpha = if (newGhosts.contains(name)) 255 else 64
+                tvId.setTextColor(Color.argb(alpha, 0, 0, 0))
             }
         }
 
         return root
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-    }
-}
-
-class CustomAdapter() : ListAdapter<String, CustomAdapter.ViewHolder>(GhostDiffCallback)  {
-    class ViewHolder(view: View) :  RecyclerView.ViewHolder(view) {
-        private val textView: TextView
-
-        init {
-            textView = view.findViewById(R.id.textRowView)
-        }
-
-        fun bind(ghost: String) {
-            textView.text = ghost;
-        }
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.text_row_layout, parent, false);
-        return ViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val ghost = getItem(position)
-        holder.bind(ghost)
-    }
-
-    object GhostDiffCallback : DiffUtil.ItemCallback<String>() {
-        override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
-            return oldItem == newItem
-        }
-
-        override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
-            return oldItem == newItem
-        }
     }
 }
--- phasmobile/src/main/java/com/example/phasmobile/ui/journal/JournalViewModel.kt
+++ phasmobile/src/main/java/com/example/phasmobile/ui/journal/JournalViewModel.kt
@@ -16,87 +16,49 @@ class JournalViewModel : BaseObservable() {
     private var _uv = false
     private var _freezing = false;
 
-    private val _ghosts = MutableLiveData<ArrayList<String>>(ArrayList())
-    val ghosts:LiveData<ArrayList<String>>
+    private val _ghosts = MutableLiveData<List<String>>(emptyList())
+
+    val ghosts:LiveData<List<String>>
         get() = _ghosts
+    init {
+        updateGhosts()
+    }
 
     init {
         updateGhosts()
     }
 
-    fun updateGhosts() {
-        val allGhosts = hashMapOf(
-            "Spirit" to true,
-            "Poltergeist" to true,
-            "Banshee" to true,
-            "Jinn" to true,
-            "Mare" to true,
-            "Revenant" to true,
-            "Shade" to true,
-            "Demon" to true,
-            "Yurei" to true,
-            "Oni" to true,
+    private fun updateGhosts() {
+        val allGhosts = arrayOf(
+            "Spirit",
+            "Poltergeist",
+            "Banshee",
+            "Jinn",
+            "Mare",
+            "Revenant",
+            "Shade",
+            "Demon",
+            "Yurei",
+            "Oni",
+            "Wraith",
+            "Phantom",
         )
 
-        if (_emf) {
-            allGhosts["Spirit"] = false
-            allGhosts["Wraith"] = false
-            allGhosts["Poltergeist"] = false
-            allGhosts["Mare"] = false
-            allGhosts["Demon"] = false
-            allGhosts["Yurei"] = false
-        }
-
-        if(_writing) {
-            allGhosts["Wraith"] = false
-            allGhosts["Phantom"] = false
-            allGhosts["Poltergeist"] = false
-            allGhosts["Banshee"] = false
-            allGhosts["Jinn"] = false
-            allGhosts["Mare"] = false
-        }
-
-        if(_uv) {
-            allGhosts["Phantom"] = false
-            allGhosts["Jinn"] = false
-            allGhosts["Mare"] = false
-            allGhosts["Shade"] = false
-            allGhosts["Demon"] = false
-            allGhosts["Yurei"] = false
-            allGhosts["Oni"] = false
-        }
-
-        if (_spiritBox){
-            allGhosts["Phantom"] = false
-            allGhosts["Banshee"] = false
-            allGhosts["Revenant"] = false
-            allGhosts["Shade"] = false
-            allGhosts["Demon"] = false
-            allGhosts["Yurei"] = false
-        }
-
-        if (_orbs){
-            allGhosts["Spirit"] = false
-            allGhosts["Wraith"] = false
-            allGhosts["Banshee"] = false
-            allGhosts["Revenant"] = false
-            allGhosts["Demon"] = false
-            allGhosts["Oni"] = false
-        }
-
-        if (_freezing){
-            allGhosts["Spirit"] = false
-            allGhosts["Poltergeist"] = false
-            allGhosts["Jinn"] = false
-            allGhosts["Revenant"] = false
-            allGhosts["Shade"] = false
-            allGhosts["Oni"] = false
-        }
-
-        _ghosts.value = ArrayList()
-        for (ghost in allGhosts.keys) {
-            if (allGhosts[ghost] == true) {
-                _ghosts.value?.add(ghost)
+        _ghosts.value = allGhosts.filter { ghost ->
+            when (ghost) {
+                "Spirit" -> !(_freezing || _emf || _orbs)
+                "Wraith" -> !(_writing || _emf || _orbs)
+                "Phantom" -> !(_uv || _writing || _spiritBox)
+                "Poltergeist" -> !(_writing || _freezing || _emf)
+                "Banshee" -> !(_writing || _spiritBox || _orbs)
+                "Jinn" -> !(_uv || _writing || _freezing)
+                "Mare" -> !(_uv || _writing || _emf)
+                "Revenant" -> !(_spiritBox || _freezing || _orbs)
+                "Shade" -> !(_uv || _spiritBox || _freezing)
+                "Demon" -> !(_uv || _emf || _orbs)
+                "Yurei" -> !(_uv || _spiritBox || _emf)
+                "Oni" -> !(_uv || _freezing || _orbs)
+                else -> false
             }
         }
     }
@@ -165,5 +127,15 @@ class JournalViewModel : BaseObservable() {
         _freezing = value
         updateGhosts()
         notifyPropertyChanged(BR.freezing)
+    }
+
+    companion object {
+        private var instance: JournalViewModel? = null
+        public fun getInstance(): JournalViewModel {
+            if (instance == null) {
+                instance = JournalViewModel()
+            }
+            return instance!!
+        }
     }
 }
